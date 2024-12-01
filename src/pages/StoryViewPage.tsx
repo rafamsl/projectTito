5d3a9ad0b7e2f74e05ad05d462aa0fcc54fa2c71
rafamsl <rafael.msl81@gmail.com>
2024-12-01 23:14:41 +0100

    story

--- src/pages/StoryViewPage.tsx
+++ src/pages/StoryViewPage.tsx
@@ -8,30 +8,8 @@ import type { Story } from '../types/story';
 export function StoryViewPage() {
   const location = useLocation();
   const navigate = useNavigate();
-  // Adicionando a história de exemplo diretamente no código
-  const defaultStory: Story = {
-    id: 3,
-    user_id: 1,
-    title: "Tito y sus nuevos amigos",
-    content: {
-      "Title": "Tito y sus nuevos amigos",
-      "1": "Había una vez una capibara llamada Tito.",
-      "2": "Tito era muy tímido y le encantaba jugar en el agua.",
-      "3": "Un día, mientras jugaba en el lago, vio a otros animales cerca.",
-      "4": "Había patitos nadando y conejitos brincando en la orilla.",
-      "5": "Tito se sintió un poco nervioso, pero también curioso.",
-      "6": "Decidió acercarse lentamente al grupo de animales.",
-      "7": "Los patitos lo saludaron con un suave cuac y los conejitos lo miraron con alegría.",
-      "8": "Tito sonrió y se unió a ellos en el agua.",
-      "9": "Jugaron juntos y se divirtieron mucho, olvidando la timidez de Tito.",
-      "10": "Al final del día, Tito había hecho nuevos amigos y se sintió muy feliz."
-    },
-    created_date: "2024-11-30T18:43:26.135161"
-  };
-
-  // Usando a história padrão se não houver história fornecida
-  // const story = (location.state?.story) as Story;
-  const story = (defaultStory) as Story;
+  
+  const story = (location.state?.story) as Story;
 
 
   return (
